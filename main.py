f18cb04572d27f44ff360eb09519bfde01c30c5f
Mikshakecere <jngobus125@gmail.com>
2025-07-01 03:47:44 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import logging
+from dotenv import load_dotenv
+from racefetcher import fetch_ct_tiles
+import os
+
+class Client(commands.Bot):
+    async def on_ready(self):
+        print(f"Logged on as {self.user}")
+
+        try:
+            guild = discord.Object(id=530935500270338058)
+            synced = await self.tree.sync(guild=guild)
+            print(f'Synced {len(synced)} commands to guild {guild.id}')
+
+        except Exception as e:
+            print(f'Error syncing commands: {e}')
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log',encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+client = Client(command_prefix="!", intents=intents)
+
+GUILD_ID = discord.Object(id=530935500270338058)
+
+@client.tree.command(name="fetch_races", description="Fetch all races for the given ct", guild=GUILD_ID)
+async def fetch_races(interaction: discord.Interaction):
+    regulars, banners, relics = fetch_ct_tiles()
+    await interaction.response.send_message(f"Regulars (" + str(len(regulars)) + "):\n" + f"{regulars}\n" +
+                                            f"Banners (" + str(len(banners)) + "):\n" + f"{banners}\n" +
+                                            f"Relics (" + str(len(relics)) + "):\n" + f"{relics}")
+
+@client.tree.command(name="fetch_relics", description="Fetch all race relevant relics for the given ct", guild=GUILD_ID)
+async def fetch_relics(interaction: discord.Interaction, ct_number: int):
+    await interaction.response.send_message(f"Placeholder text for race relevant relics from ct {ct_number}")
+
+@client.tree.command(name="fetch_all_info", description="Fetch all info for the given ct", guild=GUILD_ID)
+async def fetch_relics(interaction: discord.Interaction, ct_number: int):
+    await interaction.response.send_message(f"Placeholder text for info from ct {ct_number}")
+
+client.run(token)
+
---
+++ racefetcher.py
@@ -0,0 +1,35 @@
+import pip._vendor.requests as requests
+
+def fetch_ct_tiles():
+    # link to request info
+    url = "https://data.ninjakiwi.com/btd6/ct/"
+    ct_resp = requests.get(url).json()
+    if ct_resp['error'] is not None or ct_resp['success'] is False:
+        print("CT category unavailable at the moment")
+        exit()
+
+    # ct_id = input("Input the ct id or type 0 to fetch the latest ct: ")
+    ct_id = '0'
+
+    if ct_id == '0':
+        # fetch the latest ct id (i forgor how)
+        ct_id = ct_resp['body'][0]['id']
+
+    ct = url + ct_id + "/tiles"
+    ct_resp = requests.get(ct).json()
+    tiles = ct_resp['body']['tiles']
+    banners = []
+    relics = []
+    regular = []
+
+    for tile in tiles:
+        if tile['gameType'] == "Race":
+            if tile['type'] == "Regular":
+                regular.append(tile['id'])
+            elif tile['type'] == "Banner":
+                banners.append(tile['id'])
+            else:
+                relics.append(tile['id'])
+
+    return sorted(regular), sorted(banners), sorted(relics)
+
